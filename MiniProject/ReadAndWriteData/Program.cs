c6bd7a4d60430d90dbf8bd4749cf661a00b6f223
Yaeli Perl <yaelisstudingathome@gmail.com>
2020-10-12 17:23:59 +0200

    init commit

---
+++ MiniProject/ReadAndWriteData/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ReadAndWriteData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ MiniProject/ReadAndWriteData/TryAccess.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReadAndWriteData
+{
+    class TryAccess
+    {
+        public void Writing()
+        {
+            Console.WriteLine("Baruch Hashem It's Working!");
+        }
+    }
+}
